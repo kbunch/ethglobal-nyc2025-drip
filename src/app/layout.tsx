dcdb49f3756f41879599fc35d06a4f736c7f543c
Kenny Bunch <kenny.bunch@dreamsocket.com>
2025-08-17 02:26:48 -0400

    chg: changed metadata for the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "CDP Next.js StarterKit",
-  description: "The CDP Next.js StarterKit",
+  title: "drip - streaming payments",
+  description: "drip - streaming payments solution",
 };
 
 /**
